bdabae81ff5eef89eda0fc699a6d29589d3e1977
Devs <devennugroho18@gmail.com>
2024-05-31 20:18:00 +0700

    Create tugas.py

---
+++ tugas.py
@@ -0,0 +1,17 @@
+import random
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[2])
+
+
+name = input("Masukkan Nama Anda :")
+print("Hi ", name)
+
+
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+
+hobies = ["Main game", "Membaca Manga", "Bermain Bola"]
+foods = ["Bakso", "Nasi Goreng", "Seafood"]
+
+print("Hobi Saya: ", random.choice(hobies))
+print("Makanan Favorite Saya: ", random.choice(foods))
